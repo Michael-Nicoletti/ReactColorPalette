f3aef677ff8a62d1ae499270d03c20e54232ecd2
MichaelNicoletti <michael@13amgames.com>
2019-06-13 19:54:15 -0400

    Added the link for the single color palette

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
+import SingleColorPalette from './SingleColorPalette';
 import PaletteList from './PaletteList';
 import {generatePalette} from './colorHelpers';
 import seedColors from './seedColors';
@@ -18,7 +19,7 @@ class App extends Component {
       <Switch>
         <Route exact path='/' render={routeProps => <PaletteList palettes={seedColors} {...routeProps} />} />
         <Route exact path='/palette/:id' render={routeProps => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>} />
-        <Route exact path='/palette/:paletteId/:colorId' render={() => <h1>Single Color Page</h1>} />} />
+        <Route exact path='/palette/:paletteId/:colorId' render={() => <SingleColorPalette />} />} />
       </Switch>
     );
   }
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -19,7 +19,7 @@ export default class ColorBox extends Component {
     }
 
     render() {
-        const {name, background} = this.props;
+        const {name, background, paletteId, id} = this.props;
         const {copied} = this.state;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -35,7 +35,7 @@ export default class ColorBox extends Component {
                         </div>
                         <button className='copy-button'>Copy</button>
                     </div>
-                    <Link to='/' onClick={e => e.stopPropagation()}><span className='see-more'>More</span></Link>
+                    <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}><span className='see-more'>More</span></Link>
                 </div>
             </CopyToClipboard>
         )
