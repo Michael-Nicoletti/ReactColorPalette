8ac4c678c37a4d89676668ebf940229273aa1362
MichaelNicoletti <michael@13amgames.com>
2019-06-23 11:59:08 -0400

    Connected Color picker to add button

--- src/App.js
+++ src/App.js
@@ -17,20 +17,14 @@ class App extends Component {
 
   render() {
     return (
-      <Switch>
-        <Route exact path='/palette/new' render={() => <NewPaletteForm />} />
-        <Route exact path='/' render={routeProps => <PaletteList palettes={seedColors} {...routeProps} />} />
-        <Route exact path='/palette/:id' render={routeProps => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>} />
-        <Route 
-          exact 
-          path='/palette/:paletteId/:colorId' 
-          render={routeProps => 
-          <SingleColorPalette 
-            palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
-            colorId={routeProps.match.params.colorId}
-          />}
-        />
-      </Switch>
+		<Switch>
+			<Route exact path='/palette/new' render={() => <NewPaletteForm />} />
+			<Route exact path='/' render={routeProps => <PaletteList palettes={seedColors} {...routeProps} />} />
+			<Route exact path='/palette/:id' render={routeProps => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>} />
+			<Route exact path='/palette/:paletteId/:colorId' 
+				render={routeProps =>  <SingleColorPalette palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))} colorId={routeProps.match.params.colorId} />}
+			/>
+		</Switch>
     );
   }
   
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -74,59 +74,72 @@ const styles = theme => ({
 });
 
 class NewPaletteForm extends Component {
-    state = {
-        open: false,
-      };
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: true,
+            currentColor: 'teal',
+            colors: ['purple', '#e15764']
+        }
+    }
     
-      handleDrawerOpen = () => {
+    handleDrawerOpen = () => {
         this.setState({ open: true });
-      };
+    };
     
-      handleDrawerClose = () => {
+    handleDrawerClose = () => {
         this.setState({ open: false });
-      };
+    };
+
+    updateCurrentColor = (col) => {
+        this.setState({currentColor: col.hex});
+    }
+
+    addNewColor = () => {
+        this.setState({colors: [...this.state.colors, this.state.currentColor]});
+    }
     
-      render() {
+    render() {
         const { classes } = this.props;
         const { open } = this.state;
     
         return (
-          <div className={classes.root}>
-            <CssBaseline />
-            <AppBar position="fixed" className={classNames(classes.appBar, { [classes.appBarShift]: open, })} >
-              <Toolbar disableGutters={!open}>
-                <IconButton color="inherit" aria-label="Open drawer" onClick={this.handleDrawerOpen} className={classNames(classes.menuButton, open && classes.hide)} >
-                  <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" color="inherit" noWrap>Persistent drawer</Typography>
-              </Toolbar>
-            </AppBar>
-            <Drawer className={classes.drawer} variant="persistent" anchor="left" open={open} classes={{ paper: classes.drawerPaper, }} >
-              <div className={classes.drawerHeader}>
-                <IconButton onClick={this.handleDrawerClose}>
-                  <ChevronLeftIcon />
-                </IconButton>
-              </div>
-              <Divider />
-              <Typography variant='h4'>Design Your Palette</Typography>
-              <div>
-                <Button variant='contained' color='secondary'>Clear Palette</Button>
-                <Button variant='contained' color='primary'>Random Color</Button>
-              </div>
-              <ChromePicker color='purple' onChangeComplete={(newColor) => console.log(newColor)}/>
-              <Button variant='contained' color='primary'>Add Color</Button>
-              
-            </Drawer>
-            <main
-              className={classNames(classes.content, {
-                [classes.contentShift]: open,
-              })}
-            >
-              <div className={classes.drawerHeader} />
-            </main>
-          </div>
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar position="fixed" className={classNames(classes.appBar, { [classes.appBarShift]: open, })} >
+                    <Toolbar disableGutters={!open}>
+                        <IconButton color="inherit" aria-label="Open drawer" onClick={this.handleDrawerOpen} className={classNames(classes.menuButton, open && classes.hide)} >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>Persistent drawer</Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer className={classes.drawer} variant="persistent" anchor="left" open={open} classes={{ paper: classes.drawerPaper, }} >
+                    <div className={classes.drawerHeader}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <Typography variant='h4'>Design Your Palette</Typography>
+                    <div>
+                        <Button variant='contained' color='secondary'>Clear Palette</Button>
+                        <Button variant='contained' color='primary'>Random Color</Button>
+                    </div>
+                    <ChromePicker color={this.state.currentColor} onChangeComplete={this.updateCurrentColor}/>
+                    <Button variant='contained' color='primary' style={{backgroundColor: this.state.currentColor}} onClick={this.addNewColor}>Add Color</Button>
+                </Drawer>
+                <main className={classNames(classes.content, { [classes.contentShift]: open, })} >
+                    <div className={classes.drawerHeader} />
+                    <ul>
+                        {this.state.colors.map(col => (
+                            <li style={{backgroundColor: col}}>{col}</li>
+                        ))}
+                    </ul>
+                </main>
+            </div>
         );
-      }
+    }
 }
 
 export default withStyles(styles, {withTheme: true})(NewPaletteForm);
